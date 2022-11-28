1c6e835c8ea6823418b735c90e8c7917b5586474
luisherreralg <lherreralg@outlook.es>
2022-11-28 20:12:41 +0100

    🚧 Added cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
+import { setCors } from './middlewares/cors.js';
 
 export const app = express();
 app.disable('x-powered-by');
@@ -11,6 +12,8 @@ const corsOptions = {
 app.use(morgan('dev'));
 app.use(cors(corsOptions));
 app.use(express.json());
+
+app.use(setCors);
 
 const template = 'Work in progress';
 app.get('/', (_req, res) => {
