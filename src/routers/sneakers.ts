0d53681f5e96942cf7b3d3b1f65c5930369b6975
luisherreralg <lherreralg@outlook.es>
2022-11-29 00:59:37 +0100

    ✨ Added sneaker router

---
+++ src/routers/sneakers.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { SneakerController } from '../controllers/sneaker.js';
+import { SneakerRepository } from '../repositories/sneaker.js';
+
+export const sneakersRouter = Router();
+const controller = new SneakerController(SneakerRepository.getInstance());
+
+sneakersRouter.get('/', controller.getAll.bind(controller));
+sneakersRouter.get('/:id', controller.get.bind(controller));
+sneakersRouter.get('/search/:query', controller.search.bind(controller));
+sneakersRouter.post('/', controller.post.bind(controller));
+sneakersRouter.patch('/:id', controller.patch.bind(controller));
+sneakersRouter.delete('/:id', controller.delete.bind(controller));
