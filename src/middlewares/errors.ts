822436ce95e4bebd9ab226ae71af99ad55303964
luisherreralg <lherreralg@outlook.es>
2022-11-29 18:42:16 +0100

    ✅ Updated tests

--- src/middlewares/errors.ts
+++ src/middlewares/errors.ts
@@ -20,5 +20,7 @@ export const errorManager = (
         type: error.name,
         error: error.message,
     };
-    resp.status(status).json(result).end();
+    resp.status(status);
+    resp.json(result);
+    resp.end();
 };
