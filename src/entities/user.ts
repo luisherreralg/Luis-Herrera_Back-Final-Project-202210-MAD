67dea81ca05d1ac8818984cf7f7808057edd02bb
luisherreralg <lherreralg@outlook.es>
2022-11-29 10:40:32 +0100

    ✨ Added user entitites

---
+++ src/entities/user.ts
@@ -0,0 +1,44 @@
+import { model, Schema, Types } from 'mongoose';
+
+export type ProtoUser = {
+    name: string;
+    surname: string;
+    email: string;
+    password: string;
+    role: 'user' | 'admin';
+};
+
+export type User = {
+    id: Types.ObjectId;
+    name: string;
+    surname: string;
+    email: string;
+    password: string;
+    role: 'user' | 'admin';
+};
+
+export const userSchema = new Schema<User>({
+    name: {
+        type: String,
+        required: true,
+    },
+    surname: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: String,
+    role: String,
+});
+
+userSchema.set('toJSON', {
+    transform: (_document, returnedObject) => {
+        returnedObject.id = returnedObject._id;
+        delete returnedObject.__v;
+        delete returnedObject._id;
+        delete returnedObject.passwd;
+    },
+});
+
+export const UserModel = model<User>('User', userSchema, 'Users');
