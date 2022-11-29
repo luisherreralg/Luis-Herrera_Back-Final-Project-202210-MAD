1eebd131193f22a918c6f32faea4d9481d8f62da
luisherreralg <lherreralg@outlook.es>
2022-11-29 15:45:38 +0100

    ✅ updated sneaker.controller test

--- src/controllers/sneaker.controller.test.ts
+++ src/controllers/sneaker.controller.test.ts
@@ -16,9 +16,132 @@ describe('Given the sneaker controller', () => {
 
     const mockResponse = { sneakers: ['sneaker'] };
 
-    test('Then getAll should return an array of sneakers', async () => {
-        await sneakerController.getAll(req as Request, resp as Response, next);
-        expect(resp.json).toHaveBeenCalledWith(mockResponse);
-        expect(resp.status).toHaveBeenCalledWith(201);
+    describe('When getAll is invoked', () => {
+        test('Then it should return a json with the sneakers', async () => {
+            await sneakerController.getAll(
+                req as Request,
+                resp as Response,
+                next
+            );
+            expect(resp.status).toHaveBeenCalledWith(201);
+            expect(resp.json).toHaveBeenCalledWith(mockResponse);
+        });
+
+        test('Then it should call next with an error if the repository throws an error', async () => {
+            repo.getAll = jest.fn().mockRejectedValue(new Error('Error'));
+            await sneakerController.getAll(
+                req as Request,
+                resp as Response,
+                next
+            );
+            expect(next).toHaveBeenCalledWith(new Error('Error'));
+        });
+    });
+
+    describe('When get is invoked', () => {
+        test('Then it should return a json with the sneaker', async () => {
+            repo.get = jest.fn().mockResolvedValue('sneaker');
+            await sneakerController.get(req as Request, resp as Response, next);
+            expect(resp.status).toHaveBeenCalledWith(201);
+            expect(resp.json).toHaveBeenCalledWith({ sneakers: ['sneaker'] });
+        });
+
+        test('Then it should call next with an error if the repository throws an error', async () => {
+            repo.get = jest.fn().mockRejectedValue(new Error('Error'));
+            await sneakerController.get(req as Request, resp as Response, next);
+            expect(next).toHaveBeenCalledWith(new Error('Error'));
+        });
+    });
+
+    describe('When search is invoked', () => {
+        test('Then it should return a json with the sneakers', async () => {
+            repo.search = jest.fn().mockResolvedValue(['sneaker']);
+            await sneakerController.search(
+                req as Request,
+                resp as Response,
+                next
+            );
+            expect(resp.status).toHaveBeenCalledWith(201);
+            expect(resp.json).toHaveBeenCalledWith(mockResponse);
+        });
+
+        test('Then it should call next with an error if the repository throws an error', async () => {
+            repo.search = jest.fn().mockRejectedValue(new Error('Error'));
+            await sneakerController.search(
+                req as Request,
+                resp as Response,
+                next
+            );
+            expect(next).toHaveBeenCalledWith(new Error('Error'));
+        });
+    });
+
+    describe('When post is invoked', () => {
+        test('Then it should return a json with the sneaker', async () => {
+            repo.post = jest.fn().mockResolvedValue('sneaker');
+            await sneakerController.post(
+                req as Request,
+                resp as Response,
+                next
+            );
+            expect(resp.status).toHaveBeenCalledWith(201);
+            expect(resp.json).toHaveBeenCalledWith({ sneakers: ['sneaker'] });
+        });
+
+        test('Then it should call next with an error if the repository throws an error', async () => {
+            repo.post = jest.fn().mockRejectedValue(new Error('Error'));
+            await sneakerController.post(
+                req as Request,
+                resp as Response,
+                next
+            );
+            expect(next).toHaveBeenCalledWith(new Error('Error'));
+        });
+    });
+
+    describe('When patch is invoked', () => {
+        test('Then it should return a json with the sneaker', async () => {
+            repo.patch = jest.fn().mockResolvedValue('sneaker');
+            await sneakerController.patch(
+                req as Request,
+                resp as Response,
+                next
+            );
+            expect(resp.status).toHaveBeenCalledWith(201);
+            expect(resp.json).toHaveBeenCalledWith({ sneakers: ['sneaker'] });
+        });
+
+        test('Then it should call next with an error if the repository throws an error', async () => {
+            repo.patch = jest.fn().mockRejectedValue(new Error('Error'));
+            await sneakerController.patch(
+                req as Request,
+                resp as Response,
+                next
+            );
+            expect(next).toHaveBeenCalledWith(new Error('Error'));
+        });
+    });
+
+    describe('When delete is invoked', () => {
+        test('Then it should return a json with the sneaker', async () => {
+            repo.delete = jest.fn().mockResolvedValue('sneaker');
+            await sneakerController.delete(
+                req as Request,
+                resp as Response,
+                next
+            );
+            expect(resp.status).toHaveBeenCalledWith(201);
+            expect(resp.json).toHaveBeenCalledWith({ sneakers: ['sneaker'] });
+        });
+
+        test('Then it should call next with an error if the repository throws an error', async () => {
+            repo.delete = jest.fn().mockRejectedValue(new Error('Error'));
+            await sneakerController.delete(
+                req as Request,
+                resp as Response,
+                next
+            );
+            expect(next).toHaveBeenCalledWith(new Error('Error'));
+        });
     });
 });
