43bd70e3381a9b08773012db37de2b4172391824
luisherreralg <lherreralg@outlook.es>
2022-11-29 01:00:31 +0100

    🤡 Updated mock data

---
+++ src/utils/mocks/mocks.ts
@@ -0,0 +1,35 @@
+import { ProtoSneaker, SneakerModel } from '../../entities/sneaker.js';
+import { DbConnections } from '../db/db.connections.js';
+
+const connections = DbConnections.getInstance();
+
+export const mockSneakers: ProtoSneaker[] = [
+    {
+        brand: 'Test1',
+        model: 'Test1',
+        size: [],
+        price: 0,
+        onSalePrice: 0,
+        onSale: false,
+        stock: 0,
+        gender: 'male',
+    },
+    {
+        brand: 'Test2',
+        model: 'Test2',
+        size: [],
+        price: 0,
+        onSalePrice: 0,
+        onSale: false,
+        stock: 0,
+        gender: 'female',
+    },
+];
+
+export async function setUpSneakerCollection() {
+    await connections.dbConnect();
+    await SneakerModel.deleteMany();
+    await SneakerModel.insertMany(mockSneakers);
+    const data = await SneakerModel.find();
+    return [data[0].id, data[1].id];
+}
