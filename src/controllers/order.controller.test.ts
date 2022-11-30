3c9dcd130beee9ddc60fd6a610f2e5dfae399600
luisherreralg <lherreralg@outlook.es>
2022-11-30 21:29:47 +0100

    ✅ tested the order controller

---
+++ src/controllers/order.controller.test.ts
@@ -0,0 +1,138 @@
+import { NextFunction, Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { User } from '../entities/user';
+import { ExtraRequest } from '../middlewares/interceptors';
+import { OrderRepository } from '../repositories/order';
+import { SneakerRepository } from '../repositories/sneaker';
+import { UserRepository } from '../repositories/user';
+import { createHttpError } from '../utils/create.http.error/create.http.error';
+import { mockOrders } from '../utils/mocks/mocks';
+import { OrderController } from './order.controller';
+
+describe('Given the OrderController', () => {
+    const mockData = mockOrders;
+
+    const orderRepo = OrderRepository.getInstance();
+    const sneakerRepo = SneakerRepository.getInstance();
+    const userRepo = UserRepository.getInstance();
+    const controller = new OrderController(orderRepo, sneakerRepo, userRepo);
+    let req: Partial<ExtraRequest> = {};
+    const resp: Partial<Response> = {};
+    resp.status = jest.fn().mockReturnValue(resp);
+    resp.json = jest.fn();
+
+    const next = jest.fn() as NextFunction;
+
+    describe('When getCart its invoked', () => {
+        orderRepo.find = jest.fn().mockResolvedValue(mockData[0]);
+
+        test('If the user has a cart, it should return it', async () => {
+            req.payload = {
+                id: '6386300332f75d17ee9d62bb',
+                name: 'Luis',
+                role: 'user',
+            };
+            await controller.getCart(
+                req as ExtraRequest,
+                resp as Response,
+                next
+            );
+            expect(resp.json).toHaveBeenCalledWith({ orders: mockData[0] });
+        });
+
+        test('If the user has not a cart, it should return an empty array', async () => {
+            orderRepo.find = jest.fn().mockResolvedValue([]);
+            await controller.getCart(
+                req as ExtraRequest,
+                resp as Response,
+                next
+            );
+            expect(resp.json).toHaveBeenCalledWith({ orders: [] });
+        });
+
+        test('If req.payload is undefined it should throw an error', async () => {
+            req = {};
+            await controller.getCart(
+                req as ExtraRequest,
+                resp as Response,
+                next
+            );
+            expect(next).toHaveBeenCalledWith(
+                createHttpError(new Error('Invalid payload'))
+            );
+        });
+    });
+
+    describe('When newOrder is invoked', () => {
+        orderRepo.find = jest.fn().mockResolvedValue(mockData[0]);
+        orderRepo.post = jest.fn().mockResolvedValue(mockData[0]);
+
+        test('If all the data its okey it should return a the new order', () => {
+            req.payload = {
+                id: '6386300332f75d17ee9d62bb',
+                name: 'Luis',
+                role: 'user',
+            };
+            req.body = {
+                sneakerId: '6386300332f75d17ee9d62bb',
+                quantity: 1,
+            };
+            controller.newOrder(req as ExtraRequest, resp as Response, next);
+            expect(resp.json).toHaveBeenCalledWith({ orders: mockData[0] });
+        });
+
+        test('If there is not payload it should return an error', async () => {
+            req = {};
+            await controller.newOrder(
+                req as ExtraRequest,
+                resp as Response,
+                next
+            );
+            expect(next).toHaveBeenCalledWith(
+                createHttpError(new Error('Invalid payload'))
+            );
+        });
+
+        test('If there is not req body it should return an error', async () => {
+            req = {};
+            await controller.newOrder(
+                req as ExtraRequest,
+                resp as Response,
+                next
+            );
+            expect(next).toHaveBeenCalledWith(
+                createHttpError(new Error('Invalid payload'))
+            );
+        });
+    });
+
+    describe('When deleteOrder is invoked', () => {
+        orderRepo.delete = jest.fn().mockResolvedValue(mockData[0]);
+        orderRepo.find = jest.fn().mockResolvedValue(mockData[0]);
+
+        test('If all the data its okey it should return a the new order', () => {
+            req.payload = {
+                id: '6386300332f75d17ee9d62bb',
+                name: 'Luis',
+                role: 'user',
+            };
+            req.params = {
+                itemId: '6386300332f75d17ee9d62bb',
+            };
+            controller.deleteOrder(req as ExtraRequest, resp as Response, next);
+            expect(resp.json).toHaveBeenCalledWith({ orders: mockData[0] });
+        });
+
+        test('If there is not payload it should return an error', async () => {
+            req = {};
+            await controller.deleteOrder(
+                req as ExtraRequest,
+                resp as Response,
+                next
+            );
+            expect(next).toHaveBeenCalledWith(
+                createHttpError(new Error('Invalid payload'))
+            );
+        });
+    });
+});
