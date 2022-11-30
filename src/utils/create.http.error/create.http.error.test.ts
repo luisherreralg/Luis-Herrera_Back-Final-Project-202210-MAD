6113a986f77a6053d5aceaf22ee725d74ccbfeb3
luisherreralg <lherreralg@outlook.es>
2022-11-30 09:39:03 +0100

    ✅ Added the http error test

---
+++ src/utils/create.http.error/create.http.error.test.ts
@@ -0,0 +1,20 @@
+import { HTTPError } from '../../interfaces/error';
+import { createHttpError } from './create.http.error';
+
+describe('Given the createHttpError function', () => {
+    describe('When its invoked', () => {
+        test('Then when the error passed to the function has a "Not found id" message it should return an http error with a statusMessage = "Not Found"', () => {
+            const mockError = new Error('Not found id');
+            const result = createHttpError(mockError);
+            expect(result.statusMessage.toString()).toEqual('Not found');
+        });
+
+        test('Then when the error passed to the function has a standard message it should return an http error with a statusMessage = "Service unavailable"', () => {
+            const mockError = new Error('Error');
+            const result = createHttpError(mockError);
+            expect(result.statusMessage.toString()).toEqual(
+                'Service unavailable'
+            );
+        });
+    });
+});
