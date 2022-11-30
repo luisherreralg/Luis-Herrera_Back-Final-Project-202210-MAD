6d2d98a29c3bf612542626898a3cd39411afddd8
luisherreralg <lherreralg@outlook.es>
2022-11-30 10:19:35 +0100

    ✅ Tested the two interceptors

---
+++ src/middlewares/interceptors.test.ts
@@ -0,0 +1,54 @@
+import { NextFunction, Request, Response } from 'express';
+import { createHttpError } from '../utils/create.http.error/create.http.error';
+import { ExtraRequest, logged } from './interceptors';
+
+describe('Given the logged interceptor', () => {
+    describe('When its invoked', () => {
+        test('When the authString is empty, it should return an error', () => {
+            const req: Partial<Request> = {
+                get: jest.fn().mockReturnValueOnce(false),
+            };
+            const res: Partial<Response> = {};
+            const next: NextFunction = jest.fn();
+
+            logged(req as Request, res as Response, next);
+            expect(next).toHaveBeenCalledWith(
+                createHttpError(new Error('Not logged'))
+            );
+        });
+    });
+
+    test('Then if the readToken function reads the token and its not valid, then it should return an error', () => {
+        const req: Partial<Request> = {
+            get: jest.fn().mockReturnValueOnce('Bearer 1234'),
+        };
+        const res: Partial<Response> = {};
+        const next: NextFunction = jest.fn();
+
+        logged(req as Request, res as Response, next);
+        expect(next).toHaveBeenCalledWith(
+            createHttpError(new Error('jwt malformed'))
+        );
+    });
+
+    test('Then if the readToken function reads a correct token, it should return the payload', () => {
+        const req: Partial<ExtraRequest> = {
+            get: jest
+                .fn()
+                .mockReturnValueOnce(
+                    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODYzMDAzMzJmNzVkMTdlZTlkNjJiYiIsIm5hbWUiOiJMdWlzIiwicm9sZSI6InVzZXIiLCJpYXQiOjE2Njk3OTc3NzF9.etU9QAFmgFNvTr-eb3e3xAopVftl2_ZZOk1B01oUiGQ'
+                ),
+        };
+        const res: Partial<Response> = {};
+        const next: NextFunction = jest.fn();
+
+        logged(req as Request, res as Response, next);
+        expect(next).toHaveBeenCalled();
+        expect(req.payload).toEqual({
+            name: 'Luis',
+            id: expect.any(String),
+            iat: expect.any(Number),
+            role: 'user',
+        });
+    });
+});
